0660ed20ecf6eaa45315a6b046c7cdfad1d170e7
jose exequiel fuentes gil <jfuen099@meow.cs.fiu.edu>
2023-08-24 18:54:57 -0400

    update

--- hex.cpp
+++ hex.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 //Symbols for players
-string player_1_symbol = "\u2716", player_2_symbol = "\u25CF", not_played = "\u2B21"; //(1)
+string red_player_symbol = "\u2716", blue_player_symbol = "\u25CF", not_played = "\u2B21"; //(1) RED == cross, BLUE == circle, not_played == Hexagon
 
 //Template for printing vectors
 template<typename T>
@@ -29,128 +29,84 @@ string operator*(const int repetitionCount, const string& str) {
     return result;
 }
 
-//Implements the graph class declared using and adjacency matrix and its basic operations. Also it has a vector storing the node values
-class Graph{
-protected:
-    vector<vector<double>> adjacency_matrix;
-    vector<double> node_values;
+//Defines Player enumeration to tell who has played which movement
+enum Player{NO_WINNER= -1, NOT_PlAYED = 0, RED = 1, BLUE = 2};
 
-public:
-    Graph(vector<vector<double>> adjacency_matrix ) 
-    : adjacency_matrix(adjacency_matrix), node_values(vector<double>(adjacency_matrix.size(), 0.0)){}
-    
-    inline int vertices() {
-        return adjacency_matrix.size();}
-    
-    int edges(){
-        int n_edges = 0;
-        for (const auto & row : adjacency_matrix){
-            for(const auto & element : row){
-                if (element > 0.0){n_edges++;}
-            }
-        }
-        return n_edges;
+ostream& operator<<(ostream &os, const Player  & player){
+    switch (player){
+        case RED:
+            cout<<"Red player";
+            break;
+        case BLUE:
+            cout<<"Blue player";
+            break;
+        case NO_WINNER:
+            cout<<"No one has won yet";
+            break;
+        default:
+            break;
     }
+    return os;
+}
 
-    vector<vector<double>> adjacencyMatrix() {
-        return adjacency_matrix;}
-    
-    inline bool adjacent(int u, int v){
-        return adjacency_matrix[u][v] > 0.0;}
-    
-    vector<int> neighbors(int u){
-        assert(u < vertices());
-        vector<int> neighbors_vector;
-        for (int j = 0; j < vertices(); j++){
-            if (adjacency_matrix[u][j]>0.0){
-                neighbors_vector.push_back(j);
-            }
-        }
-        return neighbors_vector;
-    }
-    
-    inline void setNodeValue(int u, double value){
-        assert(u < vertices() && u >= 0);
-        node_values[u] = value;}
-
-    inline double getNodeValue(int u){
-        assert(u < vertices() && u >= 0);
-        return node_values[u];}
-    
-    inline void addSetEdgeValue(int u, int v, double value = 1.0){
-        assert(u < vertices() && v < vertices() && u>= 0 && v >= 0);
-        adjacency_matrix[u][v] = value;}
-    
-    inline void addSetUndirectedEdgeValue(int u, int v, double value = 1.0){
-        addSetEdgeValue(u, v, value );
-        addSetEdgeValue(v, u, value );}
-    
-    inline double getEdgeValue(int u, int v){
-        assert(u < vertices() && v < vertices() && u>= 0 && v >= 0);
-        return adjacency_matrix[u][v];}
-    
-    inline void addDeleteEdge(int u, int v){
-        addSetEdgeValue(u, v, 0.0);}
-    
-};
-
-//HexBoard class, a subclass of Graph
-class HexBoard : public Graph{
+class HexBoard {
     private:
         int size;
+        vector<vector<int>> adjacency_matrix;
+        vector<Player> node_values;
+        vector<int> nodes_to_play;
         vector<bool> DFS_labels;
+        
     public:
         HexBoard(int size): 
-            Graph(vector<vector<double>> (size * size, vector<double>(size * size, 0.0))), size(size), DFS_labels(vector<bool>( size * size , false)) {}
+            size(size), 
+            adjacency_matrix(vector<vector<int>>(size*size, vector<int>(size*size, 0))),
+            node_values(vector<Player>(adjacency_matrix.size(), NOT_PlAYED)), 
+            nodes_to_play(vector<int>(adjacency_matrix.size(), 0)),
+            DFS_labels(vector<bool>( size*size , false)) {
+                initializeHexBoard();
+                for(int i = 1; i < size*size; i++){nodes_to_play[i] = i;}
+            }
+
+        inline int vertices() {
+        return adjacency_matrix.size();}
+
+        vector<vector<int>> adjacencyMatrix() { //For debugging purposes
+            return adjacency_matrix;}
+        
+        inline bool adjacent(int u, int v){
+            return adjacency_matrix[u][v] > 0;}
+        
+        inline void setNodeValue(int u, Player value){
+            assert(u < vertices() && u >= 0);
+            node_values[u] = value;}
+
+        inline Player getNodeValue(int u){
+            assert(u < vertices() && u >= 0);
+            return node_values[u];}
+        
+        inline vector<int> getNodesToPlay(){
+            return nodes_to_play;}
+        
+        inline void addSetEdgeValue(int u, int v, int value = 1 ){
+            assert(u < vertices() && v < vertices() && u>= 0 && v >= 0);
+            adjacency_matrix[u][v] = value;}
 
         inline void resetDFSLabels(){
-            DFS_labels = vector<bool>( size * size , false);}
+            DFS_labels = vector<bool>( size*size , false);}
 
-        void printHexBoard(){
-            int tab_num = 0;
-            printf("Printing hex board of size %d x %d:\n\n", size, size);
-            /*Board symbols, prints two types of line:
-            node_line: line with the nodes using characters in (1) preamble
-            edge_line: line with the links between nodes using characters in (2)
-            */
-            
-            string base_edge_line ="\\ / " , node_link = " — ", node_line = ""; //(2)
-            string edge_line =(string)" " + (size - 1) * base_edge_line + "\\";
-
-            for (int node = 0; node < vertices(); node++ ){
-                if (getNodeValue(node) == 1.0){node_line += player_1_symbol;}//Player 1 played on that node
-                else if (getNodeValue(node) == 2.0){node_line += player_2_symbol;}//Player 2 played on that node
-                else {node_line += not_played;}//No one has played on that node
-
-                if ( (node % size) == size - 1){
-                    //Print lines
-                    cout<<node_line<<endl;
-                    if (node/size == size-1){break;} //It has printed the last line, no edge line is necessary
-                    cout<<edge_line<<endl;
-                    //Lines re-initialization (blank node line, double tabbed edge_line)
-                    edge_line = 2*(string)" " + edge_line;
-                    node_line = 2*(++tab_num)*(string)" ";
-                    }
-                else{node_line +=node_link;}
-            }
-            cout<<endl;
-        }
-
-        vector<int> neighborsColor(int u, double color){
-            assert(u < vertices() && (color == 1.0 || color == 2.0));
-            vector<int> neighbors_vector;
-            for (int j = 0; j < vertices(); j++){
-                // cout<<j<< "  "<<getNodeValue(j)<<"  "<<(getNodeValue(j) == color)<<"  "<<adjacency_matrix[u][j]<<endl;
-                if (adjacency_matrix[u][j]>0.0 && getNodeValue(j) == color){
-                    neighbors_vector.push_back(j);
-                }
-            }
-            return neighbors_vector;
-        }
+        // inline int deleteNodeToPlay(int node){
+        //     auto index = find(nodes_to_play.begin(), nodes_to_play.end(), node);
+        //     if (index != nodes_to_play.end()){
+        //         nodes_to_play.erase(index);
+        //         return 0;
+        //     }
+        //     return 1;
+        // }
 
         /*Classifies each node (hexagon) in a Hex board according to its location
         The list is for printing purposes:
-        0 - top-right corner = node_assignations[0] char array defined just before the main function
+        0 - top-right corner 
         1 - top boundary    
         2 - top-left corner
         3 - left boundary
@@ -158,7 +114,7 @@ class HexBoard : public Graph{
         5 - bottom boundary
         6 - bottom-right corner
         7 - right boundary
-        8 - middle = node_assignations[8]
+        8 - middle 
         */  
         int nodeType(int node){
             int row = node/size, column = node % size;
@@ -181,9 +137,7 @@ class HexBoard : public Graph{
             }
 
         void initializeHexBoard(){
-            int board_nodes = vertices();
-            Graph hex_board(vector<vector<double>> (board_nodes, vector<double>(board_nodes, 0.0)));
-            for (int node = 0; node < board_nodes; node++ ){
+            for (int node = 0; node < vertices(); node++ ){
                 switch (nodeType(node)){
                 case 0: //top-right corner
                     addSetEdgeValue(node, node + 1);
@@ -241,20 +195,62 @@ class HexBoard : public Graph{
             }
         }
 
+        void printHexBoard(){
+            int tab_num = 0;
+            printf("Printing hex board of size %d x %d:\n\n", size, size);
+            /*Board symbols, prints two types of line:
+            node_line: line with the nodes using characters in (1) preamble
+            edge_line: line with the links between nodes using characters in (2)
+            */
+
+            string base_edge_line ="\\ / " , node_link = " — ", node_line = ""; //(2)
+            string edge_line =(string)" " + (size - 1) * base_edge_line + "\\";
+
+            for (int node = 0; node < vertices(); node++ ){
+                if (getNodeValue(node) == RED){node_line += red_player_symbol;}//Player 1 played on that node
+                else if (getNodeValue(node) == BLUE){node_line += blue_player_symbol;}//Player 2 played on that node
+                else {node_line += not_played;}//No one has played on that node
+
+                if ( (node % size) == size - 1){
+                    //Print lines
+                    cout<<node_line<<endl;
+                    if (node/size == size-1){break;} //It has printed the last line, no edge line is necessary
+                    cout<<edge_line<<endl;
+                    //Lines re-initialization (blank node line, double tabbed edge_line)
+                    edge_line = 2*(string)" " + edge_line;
+                    node_line = 2*(++tab_num)*(string)" ";
+                    }
+                else{node_line +=node_link;}
+            }
+            cout<<endl;
+        }
+
+        vector<int> neighborsColor(int u, Player color){
+            assert(u < vertices() && (color == RED || color == BLUE));
+            vector<int> neighbors_vector;
+            for (int j = 0; j < vertices(); j++){
+                if (adjacent(u,j) && getNodeValue(j) == color){
+                    neighbors_vector.push_back(j);
+                }
+            }
+            return neighbors_vector;
+        }
+
         /*Plays a movement and returns an integer depending whether the movement is legal or not
-        Assigns a double to the adjacency matrix 1.0 for player 1 and 2.0 for player 2, 0.0 is the default unplayed value*/
-        int playMovement(string player, tuple<int, int> & movement, bool print_board = true){
+        Assigns an int to the node_values RED for player 1 and BLUE for player 2, NOT_PlAYED is the default unplayed value*/
+        int playMovement(Player player, tuple<int, int> & movement, bool print_board = true){
                 int row = get<0>(movement), column = get<1>(movement);
-                assert(player == "player 1" ||player == "player 2");
+                assert(player == RED ||player == BLUE);
                 assert(row >= 0 && row < size);
                 assert(column >= 0 && column < size);
                 int node = (size * row) + column;
 
-                if (getNodeValue(node) == 0.0){
-                    if (player == "player 1"){setNodeValue(node, 1.0);}
-                    if (player == "player 2"){setNodeValue(node, 2.0);}
+                if (getNodeValue(node) == NOT_PlAYED){
+                    if (player == RED){setNodeValue(node, RED);}
+                    if (player == BLUE){setNodeValue(node, BLUE);}
+                    // deleteNodeToPlay(node);
                     if (print_board){
-                        cout<< player + " did a movement at position ("<< row<<", "<<column <<")\n";
+                        cout<< player << " did a movement at position ("<< row<<", "<<column <<")\n";
                         printHexBoard();
                     }
                     return 0;
@@ -262,42 +258,70 @@ class HexBoard : public Graph{
                 cout<<"Invalid movement, choose another one.\n";
                 return 1;
             }
-        void DFSColor(int source, double color){
+
+        void DFSColor(int source, Player color){
             int row, column;
             assert(source >=0 && source < vertices());
             DFS_labels[source] = true;
-            vector<int> neighbors =neighborsColor(source, color);
+            vector<int> neighbors = neighborsColor(source, color);
             // cout <<neighbors<<endl;
             for(const auto & neighbor : neighbors){if (!DFS_labels[neighbor]){DFSColor(neighbor, color);} //Do DFS recursively
             }
         }
 
-        string winnerChecker(){           
+        //Tells the winner if any or no winner 
+        Player winnerChecker(){           
             for(int source = 0; source < size; source ++){
-                if (getNodeValue(source) != 1.0){continue;}
+                if (getNodeValue(source) != RED){continue;}
                 resetDFSLabels();
-                DFSColor(source, 1.0);
+                DFSColor(source, RED);
                 // cout<<DFS_labels;
                 for (int k = 0; k < size; k++){
                     if (DFS_labels[ size*(size-1) + k ]){
-                        return "player 1";
+                        return RED;
                     }
                 }
             }
             for(int k = 0, source = 0; k < size; k ++){
                 source = k*size;
-                if (getNodeValue(source) != 2.0){continue;}
+                if (getNodeValue(source) != BLUE){continue;}
                 resetDFSLabels();
-                DFSColor(source, 2.0);
+                DFSColor(source, BLUE);
                 // cout<<DFS_labels;
                 for (int k = 1; k <= size; k++){
                     if (DFS_labels[ k*size - 1]){
-                        return "player 2";
+                        return BLUE;
                     }
                 }
             }
-            return "no winner yet";
+            return NO_WINNER;
         }
+
+    
+};
+
+class AIPlayer{
+    public:
+        const Player color;
+        int n_attempts_per_move;
+
+        AIPlayer(Player color, int n_attempts_per_move = 100) : 
+        color(color),
+        n_attempts_per_move(n_attempts_per_move)
+        {}
+
+        // int nextMovement(HexBoard current_hex_board){
+        //     HexBoard test_board;
+        //     for ( auto & node_to_play : current_hex_board.getNodesToPlay()){
+        //         test_board = current_hex_board;
+        //         for(int attempt = 0; attempt < n_attempts_per_move; attempt){
+        //             FillBoard(test_board);
+        //             test_board.
+        //         }
+
+        // }
+        // return 0
+        // }
 
 };
 
@@ -305,29 +329,30 @@ int main(){
     int size = 5 ;
     HexBoard hex_board =  HexBoard(size);
     hex_board.initializeHexBoard();
-    cout<< "Example Hex board, each non hexagonal character represents each player's move.\nPlayer 1 symbol: " + player_1_symbol + " Player 2 symbol: " + player_2_symbol + "\nOriginal board:\n";
+    cout<< "Example Hex board, each non hexagonal character represents each player's move.\n Player 1 symbol: " + red_player_symbol 
+    + " Player 2 symbol: " + blue_player_symbol + " Non played symbol: " + not_played + "\nOriginal board:\n\n";
     hex_board.printHexBoard();
     cout<<"Example movements:\n";
     auto movement = make_tuple(1,4); //Pair row column
-    hex_board.playMovement((string)"player 1", movement, false);
+    hex_board.playMovement(RED, movement, false);
     movement = make_tuple(3,2);
-    hex_board.playMovement((string)"player 2", movement, false);
+    hex_board.playMovement(BLUE, movement, false);
     movement = make_tuple(3,1);
-    hex_board.playMovement((string)"player 2", movement, false);
+    hex_board.playMovement(BLUE, movement, false);
     movement = make_tuple(3,3);
-    hex_board.playMovement((string)"player 2", movement, false);
+    hex_board.playMovement(BLUE, movement, false);
     movement = make_tuple(3,0);
-    hex_board.playMovement((string)"player 2", movement, false);
+    hex_board.playMovement(BLUE, movement, false);
     movement = make_tuple(0,4);
-    hex_board.playMovement((string)"player 1", movement, false);
+    hex_board.playMovement(RED, movement, false);
     movement = make_tuple(2,3);
-    hex_board.playMovement((string)"player 1", movement, false);
+    hex_board.playMovement(RED, movement, false);
     movement = make_tuple(4,3);
-    hex_board.playMovement((string)"player 1", movement, false);
+    hex_board.playMovement(RED, movement, false);
     movement = make_tuple(3,4);
-    hex_board.playMovement((string)"player 1", movement);
+    hex_board.playMovement(RED, movement);
 
     // cout<<"neigh"<<(bool)hex_board.DFSColor(3*size+1, 1.0)<<endl;
-    cout<<(string)hex_board.winnerChecker();
+    cout<<hex_board.winnerChecker();
     return 0;
 }
